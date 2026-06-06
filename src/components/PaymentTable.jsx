0798e0ba930a98ae4a69575768dfce0cf4bb621b
patriciaokonkwo <patriciaokonkwo@walla.co.il>
2026-06-06 17:49:04 +0300

    Add aria-labels to PaymentTable

---
+++ src/components/PaymentTable.jsx
@@ -0,0 +1,12 @@
+// Updated with aria-labels for accessibility
+import React from 'react';
+
+const PaymentTable = () => {
+  return (
+    <table aria-label="Payment Table">
+      {/* Table content */}
+    </table>
+  );
+};
+
+export default PaymentTable;
